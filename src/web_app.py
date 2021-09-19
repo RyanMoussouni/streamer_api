af0560f15570736d74c8ddf42678bf924a85a181
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2021-09-19 19:47:34 +0200

    Add get_streamer

--- src/web_app.py
+++ src/web_app.py
@@ -4,9 +4,19 @@ import sqlite3
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+db_name = "streamers"
+PATH_TO_TWITCH_BIN = "/usr/local/bin/twitch"
+
+
 def query_internal_db(query: str):
     return
 def query_twitch_db(query: str):
+    return
+
+
+def to_json(query_result):
+    return
+def update_internal_db(query_result):
     return
 
 @app.route("/", methods=["GET"])
@@ -15,7 +25,19 @@ def main():
 
 @app.route("/streamers/<streamer_name>", methods=["GET"])
 def get_streamer(streamer_name: str):
-    return
+    query = "SELECT {} FROM {}"
+    res_itrn = query_internal_db(query.format(streamer_name, db_name))
+    
+    if res_itrn:
+        return to_json(res_itrn)
+    else:
+        res_twitch = query_twitch_db(query)
+        
+        if res_twitch:
+            update_internal_db(res_twitch)
+            return to_json(res_twitch)
+        else:
+            return {}
 
 @app.route("/streamers/", methods=["GET"])
 def get_streamers():
