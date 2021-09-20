8ed0b09251bb9379689c8916ac4f9c9366ee80af
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2021-09-20 09:48:20 +0200

    Add query_internal_db

--- src/web_app.py
+++ src/web_app.py
@@ -1,5 +1,6 @@
 import os
 import json
+from sqlite3.dbapi2 import Error
 
 from flask import Flask
 import sqlite3
@@ -8,23 +9,52 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 db_name = "streamers"
-PATH_TO_TWITCH_BIN = "/usr/local/bin/twitch"
 PATH_TO_DATABASE = "streamers.db"
 
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
 
-def query_internal_db(query: str):
-    return
+def query_internal_db(streamer_name: str):
+    with create_connection(PATH_TO_DATABASE) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM  {} WHERE username=?", (streamer_name,))
+
+        rows = cur.fetchall()
+        
+        if len(rows) == 0:
+            res = {}
+        elif len(rows) == 1:
+            res = {}
+            username, streamingPlatform, streamingURL, profilePictureURL = rows[0]
+            res["username"] = username
+            res["streamingPlatform"] = streamingPlatform
+            res["streamingURL"] = streamingURL
+            res["profilePictureURL"] = profilePictureURL
+        else:
+            raise Exception("Dupplicate data in the database")
+    return res
 def query_twitch_db(streamer_name: str):
     command = "twitch api get users -q login={}".format(streamer_name)
     response = json.loads(os.popen(command).read())
 
-    res = response["data"]
+    try:
+        res = {}
+        res["steamingPlatform"] = "twitch"
+        res["username"] = response["data"][0]["display_name"]
+        res["profilePictureURL"] = response["data"][0]["profile_image_url"]
+        res["streamingURL"] = "twitch.tv/{}".format(streamer_name)
+    except IndexError:
+        #no data on the streamer
+        res = {}
     
-    return
+    return res
 
 
-def to_json(query_result):
-    return
 def update_internal_db(query_result):
     return
 
@@ -34,17 +64,16 @@ def main():
 
 @app.route("/streamers/<streamer_name>", methods=["GET"])
 def get_streamer(streamer_name: str):
-    query = "SELECT * FROM {} WHERE username=?".format(db_name)
-    res_itrn = query_internal_db(query)
+    res_itrn = query_internal_db(streamer_name)
     
     if res_itrn:
-        return to_json(res_itrn)
+        return res_itrn
     else:
         res_twitch = query_twitch_db(streamer_name)
         
-        if res_twitch:
+        if res_twitch != {}:
             update_internal_db(res_twitch)
-            return to_json(res_twitch)
+            return res_twitch
         else:
             return {}
 
