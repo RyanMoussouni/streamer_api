a11a653d0cb666cde24dce9a24733d084dbd3f9e
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2021-09-20 09:27:56 +0200

    Add query twitch db

--- src/web_app.py
+++ src/web_app.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 from flask import Flask
 import sqlite3
 
@@ -6,11 +9,17 @@ app.config["DEBUG"] = True
 
 db_name = "streamers"
 PATH_TO_TWITCH_BIN = "/usr/local/bin/twitch"
+PATH_TO_DATABASE = "streamers.db"
 
 
 def query_internal_db(query: str):
     return
-def query_twitch_db(query: str):
+def query_twitch_db(streamer_name: str):
+    command = "twitch api get users -q login={}".format(streamer_name)
+    response = json.loads(os.popen(command).read())
+
+    res = response["data"]
+    
     return
 
 
@@ -25,13 +34,13 @@ def main():
 
 @app.route("/streamers/<streamer_name>", methods=["GET"])
 def get_streamer(streamer_name: str):
-    query = "SELECT {} FROM {}"
-    res_itrn = query_internal_db(query.format(streamer_name, db_name))
+    query = "SELECT * FROM {} WHERE username=?".format(db_name)
+    res_itrn = query_internal_db(query)
     
     if res_itrn:
         return to_json(res_itrn)
     else:
-        res_twitch = query_twitch_db(query)
+        res_twitch = query_twitch_db(streamer_name)
         
         if res_twitch:
             update_internal_db(res_twitch)
